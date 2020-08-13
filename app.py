675f41886da5d6c37f4ff1a129f0472760c9d92a
Josh Yap <joshua.c.yap@gmail.com>
2020-08-12 22:42:09 -0500

    setting up aws lambda functions and testing

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-import tweepy 
+import tweepy
+from os import environ
 # tweepy documentation: http://docs.tweepy.org/en/latest/
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+api.update_status('its johnny')
+
